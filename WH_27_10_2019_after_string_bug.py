b2584c3d971bacb4abc72f4151314eff3ea85a54
KNiemyska <48437664+KNiemyska@users.noreply.github.com>
2019-10-31 23:22:17 +0100

    Add files via upload
    Language: only english
    Imports: before coding
    User can play more than 2
    Reading file before evry game

--- WH_27_10_2019_after_string_bug.py
+++ WH_27_10_2019_after_string_bug.py
@@ -23,72 +23,67 @@
 # wyświetl rundę
 # 6)sprawdź ktróa to runda
 
-def wisielec_game():
+import time
+import os
+def dictionary():
     my_file = open("dictionary_climbing.txt", "r")
     data = my_file.readlines()
     words = []
-    import time
-    import os
     for line in data:
         stripped_line = line.strip()
         words.append(stripped_line)
+    return words
+    my_file.close()
+def hangman_game():
+    words=dictionary()
     from random import randint
-    random_number = randint(1, len(words))  # losowanie liczby
-    selected_word = (words[random_number - 1]).lower()  # wybranie losowego słowa
-    plansza = ["_ "] * len(selected_word)  # wyświetlenie planszy
-    print("".join(plansza))  # wydrukowanie planszy
+    random_number = randint(1, len(words))  # drawing a number
+    selected_word = (words[random_number - 1]).lower()  # choosing selected word
+    board = ["_ "] * len(selected_word)  # creating boars
+    print("".join(board))  # printing boars
     attempt = int(len(selected_word) + 2)
-    szansa = 0  # liczba prób
-    runda = 1  # liczba rund
+    chance = 0  # amount of chances
+    round = 1  # amount of rounds
     selected_letter = []
     selected_word_list = []
     for char in selected_word:
         selected_word_list += [char]
-    print(f"to Twoja próba nr {runda}, pozostało Ci {attempt} szans!!\n")
-    runda += 1
-    while (szansa) < (attempt):
-        try_user = (input("\n podaj literę    ")).lower()
+    print(f"this is Your round number {round}, You have {attempt} chances left!!\n")
+    round += 1
+    while (chance) < (attempt):
+        try_user = (input("enter the letter   ")).lower()
         if try_user in selected_letter:
-            try_user = input(" HALO HALO, tą literkę już podawałeś, wybierz inną \n")
+            try_user = input(" HALO HALO, you have already entered this letter, choose another one\n")
         if try_user in selected_word:
             for elements in range(0, len(selected_word)):
                 if selected_word[elements] == try_user:
-                    plansza[elements] = try_user
-            print("".join(plansza))
-            if plansza == selected_word_list:
-                print(f"brawo, WYGRAŁEŚ odgadnięte hasło to {selected_word}")
+                    board[elements] = try_user
+            print("".join(board))
+            if board == selected_word_list:
+                print(f"CONGRATULATIONS!!!, YOU won! The guessed word is {selected_word}")
                 time.sleep(2)
                 break
         else:
-            print(f"niestety skucha, wybierz inną literkę")
-            szansa += 1
-        print(f"\n to Twoja próba nr {runda}, pozostało Ci {attempt - (szansa)} szans!!\n")
-        runda += 1
+            print(f"sorry, choose a different letter")
+            chance += 1
+        print(f"\n it is Your round number {round}, You have {attempt - chance} chances left!!\n")
+        round += 1
         selected_letter.append(try_user)
 
-    if attempt == szansa:
-        print(f"niestety PAPA! już wykorzystałeś {szansa} szans - PRZEGRAŁEŚ\n szukanym słowem było {selected_word}")
+    if attempt == chance:
+        print(f"Unfortunately bye bye! you have already take all {chance}chances - you LOST \n the word you were looking for was {selected_word}")
         time.sleep(2)
-    my_file.close()
 
-
-import os
-import time
-
+    next_try = input("chcesz jeszcze raz zagrać? [t/n]")
+    if next_try == "t":
+        os.system("cls")
+        print("BACK IN THE GAME!\n")
+        hangman_game()
+    else:
+        print("It's a pity. See you next time!")
+        time.sleep(3)
 print(
-    "Witaj, zaraz zagrasz w wiśielca""\n maszyna wylosowała dla Ciebie hasło\n poniżej zobaczysz planszę. \n")  # przywitanie użytkownika
-# time.sleep(3)  # BUG: chcę żeby użytkownik nacisnął enter i dopiero dalej przeszedł, nie używać funkcji time
-os.system("pause")
-os.system("cls")
-wisielec_game()
-next_try = input("chcesz jeszcze raz zagrać? [t/n]")
-if next_try == "t":
-    os.system("cls")
-    print("\nWITAJ PONOWNIE W GRZE!\n")
-    wisielec_game()
-else:
-    print("Szkoda, do zobaczenia!")
-    time.sleep(3)
+    "Hello, you are going to play in a hangman\n the machine has drawn a password for you \n below you will see the board.")
+input("Are You ready? To continue click enter")
+hangman_game()
 
-# dodać czy chcesz zagrać jeszcze raz
-# czysczenie panelu
